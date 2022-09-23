5430cb4d9194895a490f391f2a852614d9576bf1
SahadevIngule <sahadevingule21@gmail.com>
2022-09-23 22:54:19 +0530

    File Added

--- Github/src/List/a123.java
+++ Github/src/List/a123.java
@@ -5,6 +5,8 @@ public class a123 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sjdskdfjosdfjdjfgdgj");
+		System.out.println("s jdfhdsi djsdf jdsijf djds");
+		System.out.println("sjdskdfjosdfjdjfgdgj");
 	}
 
 }
